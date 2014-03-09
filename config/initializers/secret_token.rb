b8d1da6cc4c09795b250362083e2b99dbeb22a5d
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2014-03-09 14:56:49 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ThatStatBook::Application.config.secret_key_base = 'b54b2bb1dc8acb03ec23d4e8150f51d19fb696353f53ed65aadccd21ae913b75506fc6c1952e105e8a1e4a59f6c46a52d991544e6f84c41d28f22ece68f6e880'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ThatStatBook::Application.config.session_store :cookie_store, key: '_that_stat_book_session'
