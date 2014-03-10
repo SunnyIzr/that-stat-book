266897a51f2c1ecf8c6d39604648c335fb85d37c
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2014-03-10 18:36:23 -0400

    updating capi script

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,58 +1,61 @@
-# config valid only for Capistrano 3.1
-lock '3.1.0'
+default_run_options[:pty] = true
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+require 'bundler/capistrano'
+set :application, "thatstatbook"
+set :repository, "Users/sunnyisrani/Dropbox/apps/that-stat-book"
+set :deploy_to, "/var/www/#{application}" #path to your app on the production server
 
-# Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+set :scm, :git
+set :branch, "master"
+set :deploy_via, :copy
+set :shallow_clone, 1
 
-# Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
+set :user, "deploy" #this is the ubuntu user we created
+set :password, "secret" #deploy's password
+set :use_sudo, false
 
-# Default value for :scm is :git
-# set :scm, :git
+set :mysql_user, "deploy" #this is the mysql user we created
+set :mysql_password, "secret"
 
-# Default value for :format is :pretty
-# set :format, :pretty
+set :domain, 'thatstatbook.com'
+role :web, domain
+role :app, domain
+role :db,  domain, :primary => true
 
-# Default value for :log_level is :debug
-# set :log_level, :debug
+after "deploy:restart", "deploy:cleanup"
 
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
-
-# Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
+#Passenger
 namespace :deploy do
-
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
-    end
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
-
-  after :publishing, :restart
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
 end
+
+after "deploy:setup", "db_yml:create"
+after "deploy:update_code", "db_yml:symlink"
+
+namespace :db_yml do
+  desc "Create database.yml in shared path"
+  task :create do
+    config = {
+              "production" =>
+              {
+                "adapter" => "mysql2",
+                "socket" => "/var/run/mysqld/mysqld.sock",
+                "username" => mysql_user,
+                "password" => mysql_password,
+                "database" => "#{application}_production"
+              }
+            }
+    put config.to_yaml, "#{shared_path}/database.yml"
+  end
+
+  desc "Make symlink for database.yml"
+  task :symlink do
+    run "ln -nfs #{shared_path}/database.yml #{release_path}/config/database.yml"
+  end
+end
+
+
