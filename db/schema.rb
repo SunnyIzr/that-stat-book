6ee5b9a0a731fde54c431ac7904b2abfcadee2df
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2014-03-10 21:41:49 -0400

    setting up lessons model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140311013228) do
+ActiveRecord::Schema.define(version: 20140311014006) do
+
+  create_table "lessons", force: true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string "first_name"
