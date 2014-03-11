fb90b456ec5b21723951157d0f3f1e7f07806e14
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2014-03-10 21:39:05 -0400

    removing ds_store from git

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140311013228) do
+
+  create_table "users", force: true do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+  end
 
 end
