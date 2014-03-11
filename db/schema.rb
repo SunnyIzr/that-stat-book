756fecb7914ea3000b9ad50eb37abc852e839644
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2014-03-10 21:46:06 -0400

    creating quiz model

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140311014006) do
+ActiveRecord::Schema.define(version: 20140311014425) do
 
   create_table "lessons", force: true do |t|
     t.string   "title"
     t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "quizzes", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
