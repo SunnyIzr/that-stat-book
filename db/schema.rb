21b468c537765a19006cf8479b442ebc3ece14f4
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2014-03-10 21:50:36 -0400

    Creating choices model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140311014648) do
+ActiveRecord::Schema.define(version: 20140311014840) do
+
+  create_table "choices", force: true do |t|
+    t.text     "choice"
+    t.boolean  "is_correct"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "lessons", force: true do |t|
     t.string   "title"
