43dbb4eb91291e93528d7cec2b596aaf87c7eb16
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2014-03-12 15:34:10 -0400

    adding validations for quiz model

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,7 +1,9 @@
 class Quiz < ActiveRecord::Base
-belongs_to :user
-belongs_to :lesson
-has_many :questions
-has_many :answer_submissions
+  belongs_to :user
+  belongs_to :lesson
+  has_many :questions
+  has_many :answer_submissions
+  validates_presence_of :user_id
+  validates_presence_of :lesson_id
 
 end
--- spec/models/quiz_spec.rb
+++ spec/models/quiz_spec.rb
@@ -4,4 +4,6 @@ describe Quiz do
   it {should belong_to (:user)}
   it {should belong_to (:lesson)}
   it {should have_many (:answer_submissions)}
+  it {should validate_presence_of (:user_id)}
+  it {should validate_presence_of (:lesson_id)}
 end
