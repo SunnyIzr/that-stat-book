e2aa7c3877ec1cbaf6c9fb6404cb1a57df6c71be
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2014-03-12 19:07:21 -0400

    adding scoring behaviors to quiz model

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -14,4 +14,17 @@ class Quiz < ActiveRecord::Base
   def answered_questions
     self.answer_submissions.map {|ans| ans.choice.question}
   end
+
+  def complete?
+    self.answer_submissions.size == 5
+  end
+
+  def score
+    scores = self.answer_submissions.map { |ans| ans.choice.is_correct ? 1 : 0 }
+    scores.inject{ |sum,ans_score| sum + ans_score }.to_f / scores.size
+  end
+
+  def pass?
+    self.score >= 0.7
+  end
 end
