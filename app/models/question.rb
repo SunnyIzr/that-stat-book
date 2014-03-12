461016a0f44e151c553853a928f291c0ec0baf36
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2014-03-12 18:45:22 -0400

    return random question that hasnt been answered

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,7 +5,14 @@ class Question < ActiveRecord::Base
   validates_presence_of :lesson_id
 
   def self.random(lesson_id,quiz_id=nil)
-    questions = self.where(lesson_id: lesson_id)
-    questions.sample
+    self.available_questions(lesson_id,quiz_id).sample
+  end
+
+  def self.available_questions(lesson_id,quiz_id)
+    all_questions = self.where(lesson_id: lesson_id)
+    unless quiz_id.nil?
+      all_questions -= Quiz.find(quiz_id).answered_questions
+    end
+    all_questions
   end
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -16,4 +16,27 @@ describe Question do
     expect(Question.random(lesson.id).lesson).to eq(lesson)
 
   end
+
+  it 'should return a list of available questions for a specified lesson that does NOT include questions that have been answered' do
+    user = FactoryGirl.create(:user)
+    10.times do
+      lesson = FactoryGirl.create(:lesson)
+      10.times { lesson.questions << FactoryGirl.create(:question,:lesson_id => lesson.id)}
+      lesson.questions.each { |question| 5.times { question.choices << FactoryGirl.create(:choice, :question_id => question.id) } }
+      quiz = FactoryGirl.create(:quiz, :lesson_id => lesson.id, :user_id => User.last.id)
+      3.times { |i| FactoryGirl.create(:answer_submission,:choice_id => lesson.questions[i].choices.sample.id, :quiz_id => quiz.id)}
+    end
+    p '*'*50
+    p Lesson.first
+    p Lesson.first.quizzes.first.answered_questions
+    p '*'*50
+    p Lesson.first.quizzes.first.answered_questions.size
+
+
+    expect(Question.available_questions(Lesson.first.id,Lesson.first.quizzes.first.id)).should_not include(Lesson.first.quizzes.first.answered_questions[0])
+    expect(Question.available_questions(Lesson.first.id,Lesson.first.quizzes.first.id)).should_not include(Lesson.first.quizzes.first.answered_questions[1])
+    expect(Question.available_questions(Lesson.first.id,Lesson.first.quizzes.first.id)).should_not include(Lesson.first.quizzes.first.answered_questions[2])
+
+
+  end
 end
