89f04bfd4862f8cb298285f169004f283255a741
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2014-03-12 15:38:20 -0400

    adding validations for answer submission model

--- app/models/answer_submission.rb
+++ app/models/answer_submission.rb
@@ -1,5 +1,7 @@
 class AnswerSubmission < ActiveRecord::Base
-belongs_to :quiz
-belongs_to :choice
+  belongs_to :quiz
+  belongs_to :choice
+  validates_presence_of :quiz_id
+  validates_presence_of :choice_id
 
 end
--- spec/models/answer_submission_spec.rb
+++ spec/models/answer_submission_spec.rb
@@ -3,4 +3,6 @@ require 'spec_helper'
 describe AnswerSubmission do
   it {should belong_to (:quiz)}
   it {should belong_to (:choice)}
+  it {should validate_presence_of (:quiz_id)}
+  it {should validate_presence_of (:choice_id)}
 end
