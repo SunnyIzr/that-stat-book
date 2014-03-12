b491f160f0a8b106dfeb6c15bcb83efc18ff32d6
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2014-03-12 15:08:10 -0400

    adding validations for lesson to ensure title

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,4 +1,5 @@
 class Lesson < ActiveRecord::Base
   has_many :questions
   has_many :quizzes
+  validates_presence_of :title
 end
--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -3,4 +3,5 @@ require 'spec_helper'
 describe Lesson do
   it {should have_many (:questions)}
   it {should have_many (:quizzes)}
+  it {should validate_presence_of (:title)}
 end
