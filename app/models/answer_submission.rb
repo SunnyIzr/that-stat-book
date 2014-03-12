0770629681a8117a8dea3535e488d91f838a7e2b
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2014-03-12 16:37:48 -0400

    ensuring that a quiz does not get answers more than once from a single quesiton

--- app/models/answer_submission.rb
+++ app/models/answer_submission.rb
@@ -3,5 +3,14 @@ class AnswerSubmission < ActiveRecord::Base
   belongs_to :choice
   validates_presence_of :quiz_id
   validates_presence_of :choice_id
+  validate :question_cannot_be_answered_twice
+
+  def question_cannot_be_answered_twice
+    question = self.choice.question
+    questions_answered = self.quiz.answer_submissions.map { |ans| ans.choice.question }
+    if questions_answered.include?(question)
+      errors.add(:choice, 'Question has aready been answered in this Quiz.')
+    end
+  end
 
 end
