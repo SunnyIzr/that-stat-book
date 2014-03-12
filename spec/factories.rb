5df2a2664c3d671c443581dd700c4865e50d9569
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2014-03-12 16:05:01 -0400

    adding factories

---
+++ spec/factories.rb
@@ -0,0 +1,21 @@
+FactoryGirl.define do
+
+  factory :user do
+  end
+
+  factory :lesson do
+  end
+
+  factory :question do
+  end
+
+  factory :choice do
+  end
+
+  factory :quiz do
+  end
+
+  factory :answer_submission do
+  end
+
+end
