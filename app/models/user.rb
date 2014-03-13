bbafa11d9c083a0244f590b8819af19f27f68cb5
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2014-03-12 20:04:00 -0400

    devising user model and migrating all devise columns for database

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   has_many :quizzes
   validates_presence_of :email
   validates_uniqueness_of :email, :case_sensitive => false
