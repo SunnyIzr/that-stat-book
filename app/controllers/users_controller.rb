1bc2e263e8642429ac0aca1f052e6553dee1fb4e
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2014-03-12 23:44:04 -0400

    creating seperate admin/user dashboards

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
   def dashboard
     @user = current_user
+    if current_user.admin?
+      render :admin_dashboard
+    else
+      render :user_dashboard
+    end
   end
 end
