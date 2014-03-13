0dacd45f1f191a2d25e20a3a3f06cd0de8172754
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2014-03-13 19:25:41 -0400

    ensuring that quiz id is present when answering a question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,6 +6,6 @@ class QuestionsController < ApplicationController
   def show_random_question
     @quiz = Quiz.find(params[:quiz_id])
     @question = @quiz.new_random_question
-    redirect_to question_path(@question)
+    redirect_to ("/questions/#{@question.id}?quiz_id=#{@quiz.id}")
   end
 end
