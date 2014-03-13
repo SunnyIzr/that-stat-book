64c357f5120318ff9062618e20e94646cfd8e21e
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2014-03-13 19:57:20 -0400

    restricting lesson view if user does not have access

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -2,5 +2,10 @@ class LessonsController < ApplicationController
   def show
     @lesson = Lesson.find(params[:id])
     @quiz = Quiz.new
+    if current_user.access?(@lesson.level)
+      render :show
+    else
+      render :restricted
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ ThatStatBook::Application.routes.draw do
   resources :answer_submissions, only: [:create]
 
   get '/quizzes/:quiz_id/new-question' => 'questions#show_random_question'
+  get '/lessons/restricted' => 'lessons#restricted'
 
 
 
