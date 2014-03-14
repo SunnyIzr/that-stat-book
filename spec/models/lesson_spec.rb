8e73b35ad6e3a06caf65379f0bbb19b37eabe589
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2014-03-13 22:56:46 -0400

    added video validations with testing

--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe Lesson do
   it {should have_many (:questions)}
   it {should have_many (:quizzes)}
+  it {should have_many (:videos)}
   it {should validate_presence_of (:title)}
   it {should validate_presence_of (:level)}
   it {should validate_uniqueness_of (:level)}
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
 
 describe Video do
+  it {should belong_to(:lesson)}
 end
