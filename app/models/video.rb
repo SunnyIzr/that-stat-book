db425f5db4ed859f7cf6c72ac02a3100d0461273
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2014-04-01 18:24:22 -0400

    moving paperclip setup to host on amazon s3

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,11 @@
 class Video < ActiveRecord::Base
   belongs_to :lesson
-  has_attached_file :video, styles: { original: "4000x4000>", small: "200x200>", thumb: "100x100>" }
+  has_attached_file :video,
+                    :storage => :s3,
+                    :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
+  do_not_validate_attachment_file_type :video
+
+  def s3_credentials
+    {:bucket => ENV['BUCKET'], :access_key_id => ENV['ACCESS_KEY_ID'], :secret_access_key => ENV['SECRET_ACCESS_KEY']}
+  end
 end
