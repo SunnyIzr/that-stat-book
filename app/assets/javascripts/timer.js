252e8bc2ba46bb581035ed544b4159efe8129b88
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2014-04-01 20:27:00 -0400

    ensure that timer is adding seconds to pre-existing start time

--- app/assets/javascripts/timer.js
+++ app/assets/javascripts/timer.js
@@ -1,16 +1,22 @@
 var TimerController = {
   init: function() {
+    this.setQuizStartTime()
     this.startTimer()
+  },
+  setQuizStartTime: function() {
+    TimerModel.quizStartTime = parseInt($('.time')[0].value)
   },
   startTimer: function() {
     setInterval(function() {
-    $('.time').text( parseInt( (new Date - TimerModel.startTime) / 1000 ));
+    $('.timer').text( TimerModel.quizStartTime + parseInt( (new Date - TimerModel.startTime) / 1000 ));
+    $('.time')[0].value = TimerModel.quizStartTime + parseInt( (new Date - TimerModel.startTime) / 1000 );
 }, 1000);
   }
 }
 
 var TimerModel = {
-  startTime: new Date
+  startTime: new Date,
+  quizStartTime: 0
 }
 
 var TimerView = {
