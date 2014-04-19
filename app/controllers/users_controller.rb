1a964f4caef46b2da8a136c874b78f11d031b4a3
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2014-04-19 11:55:43 -0400

    adding user dashboard styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,19 @@
 class UsersController < ApplicationController
   def dashboard
     @user = current_user
+    @quiz = Quiz.new
     if current_user.admin?
       render :admin_dashboard
     else
       render :user_dashboard
+    end
+  end
+  def summary
+    @user = current_user
+    if current_user.admin?
+      render :admin_summary
+    else
+      render :summary
     end
   end
 end
