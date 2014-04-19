814a9c792655d26d763cba8d51278d09e53c2ef1
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2014-04-19 16:24:37 -0400

    adding fields to user table

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    home_path
+    user_dashboard_path
   end
 
   protected
