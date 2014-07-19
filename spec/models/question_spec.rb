3f065f50983e9313b2bc665f87197b2ea3498225
Sunny Israni <sunny@zoraab.com>
2014-07-19 15:49:51 -0400

    adding more tests for question model

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -13,4 +13,9 @@ describe Question do
 
     expect(question.answer).to eq(answer)
   end
+  
+  it 'should return a hash containing all amazon s3 credentials; this is used for attaching optional image file for a given quesiton' do
+    question = FactoryGirl.create(:question)
+    expect(question.s3_credentials).to eq({:bucket => ENV['BUCKET'], :access_key_id => ENV['ACCESS_KEY_ID'], :secret_access_key => ENV['SECRET_ACCESS_KEY']})
+  end
 end
