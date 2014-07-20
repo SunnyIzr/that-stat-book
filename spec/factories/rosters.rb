36d4bd1f16a71ab7890571f65d99e74fefcd0097
Sunny Israni <sunny@zoraab.com>
2014-07-20 16:04:51 -0400

    another factories typo

--- spec/factories/rosters.rb
+++ spec/factories/rosters.rb
@@ -3,6 +3,6 @@
 FactoryGirl.define do
   factory :roster do
     title {'Test Roster'}
-    roster { Professor.last.nil? ? FactoryGirl.create(:professor) : Professor.last}
+    professor { Professor.last.nil? ? FactoryGirl.create(:professor) : Professor.last}
   end
 end
