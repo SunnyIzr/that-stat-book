07f39dfd6dacbe6d00886193c78bd3eac741790a
Sunny Israni <sunny@zoraab.com>
2014-07-20 16:01:50 -0400

    typo in factories

--- spec/factories/rosters.rb
+++ spec/factories/rosters.rb
@@ -1,7 +1,7 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :professor do
+  factory :roster do
     title {'Test Roster'}
     roster { Professor.last.nil? ? FactoryGirl.create(:professor) : Professor.last}
   end
