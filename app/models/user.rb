b739c77ab7b19daaae861b9c38f8182ee6cd34c9
Sunny Israni <sunny@zoraab.com>
2014-07-20 15:48:52 -0400

    rearranging code location

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,10 +19,6 @@ class User < ActiveRecord::Base
   def completed_quizzes
     self.quizzes.select {|quiz| quiz.complete?}
   end
-  
-  def completed_quizzes_by_lesson(lesson_id)
-    self.completed_quizzes.select { |quiz| quiz.lesson_id == lesson_id }
-  end
 
   def passed_quizzes
     self.completed_quizzes.select {|quiz| quiz.pass?}
@@ -44,6 +40,17 @@ class User < ActiveRecord::Base
     self.completed_levels.max.to_i + 1
   end
 
+  def update_belts
+    unless self.completed_lessons.empty?
+      belt = self.completed_lessons.last.belt
+      lessons = belt.lessons
+      passes = lessons.map { |lesson| self.completed_lessons.include?(lesson) }
+      self.belts << belt unless passes.include?(false)
+      self.belts.uniq!
+      self.save
+    end
+  end
+  
   def access?(level)
     accessible = self.completed_levels
     accessible << self.level
@@ -60,15 +67,8 @@ class User < ActiveRecord::Base
     incomplete_quizzes.last
   end
   
-  def update_belts
-    unless self.completed_lessons.empty?
-      belt = self.completed_lessons.last.belt
-      lessons = belt.lessons
-      passes = lessons.map { |lesson| self.completed_lessons.include?(lesson) }
-      self.belts << belt unless passes.include?(false)
-      self.belts.uniq!
-      self.save
-    end
+  def completed_quizzes_by_lesson(lesson_id)
+    self.completed_quizzes.select { |quiz| quiz.lesson_id == lesson_id }
   end
   
   def avg_score(lesson_id)
@@ -80,11 +80,7 @@ class User < ActiveRecord::Base
       sum / lesson_quizzes.size
     end
   end
-  
-  def ninja_status
-    self.completed_lessons.size.to_f / Lesson.all.size
-  end
-  
+    
   def view_count(lesson_id)
     lesson = Lesson.find(lesson_id)
     if lesson.videos.empty?
@@ -93,6 +89,10 @@ class User < ActiveRecord::Base
       video_id = lesson.videos.first.id
       self.video_views.where(video_id: video_id).size
     end
+  end
+
+  def ninja_status
+    self.completed_lessons.size.to_f / Lesson.all.size
   end
 
 end
