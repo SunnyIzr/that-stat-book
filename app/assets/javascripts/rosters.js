07297b5b5dc1fa4126f355353c62046fd2019958
Sunny Israni <sunny@zoraab.com>
2014-07-29 10:42:21 -0400

    adding live search when adding students and fixing formatting issues in firefox

--- app/assets/javascripts/rosters.js
+++ app/assets/javascripts/rosters.js
@@ -3,7 +3,27 @@ $(function() {
 })
 
 var studentSelect = function() {
-  $('.existing-students option').dblclick(function() {
+  $(document).on('dblclick','.existing-students option',function() {
     $('.confirmed-students').append(this)
   })
 }
+
+
+$(function() {
+  var opts = $('#student-list option').map(function(){
+    return [[this.value, $(this).text()]];
+  });
+
+
+  $('#student-search').keyup(function(){
+    var rxp = new RegExp($('#student-search').val(), 'i');
+    var optlist = $('#student-list').empty();
+    opts.each(function(){
+      if (rxp.test(this[1])) {
+        optlist.append($('<option/>').attr('value', this[0]).text(this[1]));
+      }
+    });
+    
+  });
+  
+});
