bfa68c3437abb19129384c496520aa04e52ee259
Sunny Israni <sunny@zoraab.com>
2014-10-26 15:31:20 -0400

    professor now has many class requests

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -1,3 +1,4 @@
 class Professor < User
   has_many :rosters
+  has_many :class_requests, through: :rosters
 end
--- spec/models/professor_spec.rb
+++ spec/models/professor_spec.rb
@@ -2,4 +2,5 @@ require 'spec_helper'
 
 describe Professor do
   it {should have_many (:rosters)}
+  it {should have_many (:class_requests)}
 end
