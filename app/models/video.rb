aa899c8ee3bd51007e0caf99527491fc9346cd76
Sunny Israni <sunny@zoraab.com>
2015-01-07 19:45:07 -0500

    moving over to a secure protocol on amzn videos since it wasn't allowing to load multiple videos at once on unsecure protocol

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,10 +2,12 @@ class Video < ActiveRecord::Base
   belongs_to :lesson
   has_attached_file :video,
                     :storage => :s3,
-                    :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
+                    :s3_credentials => Proc.new{|a| a.instance.s3_credentials },
+                    :s3_protocol => 'https'
   has_attached_file :ogv_video,
                     :storage => :s3,
-                    :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
+                    :s3_credentials => Proc.new{|a| a.instance.s3_credentials },
+                    :s3_protocol => 'https'
   do_not_validate_attachment_file_type :video
 
   def s3_credentials
